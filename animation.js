943be9ba535083e999bbd0f8adedc98678b54a62
Kai Zhou <kazh03@acadsoc.com>
2019-10-29 18:24:45 +0800

    动画属性说明

---
+++ animation.js
@@ -0,0 +1,16 @@
+/**
+ * animation动画的常用参数说明：
+ * 
+ * animation  name
+ * animation  duration
+ * animation timing-function 
+ * animation delay-time
+ * animation interation-count  n|inflite（无数次）
+ * animation direction   normal|alternamte  (反向)
+ * animation plaing-state  播放的状态   running 和 paused
+ * anmation fill-mode  保存动画的状态  none forward  backward both 
+ * 
+ * 
+ * 
+ * 
+ * */ 
